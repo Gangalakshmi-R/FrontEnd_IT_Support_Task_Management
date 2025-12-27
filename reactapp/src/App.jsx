9305c612bcebdef978865078c4ba3cfe96e8af19
Gangalakshmi-R <glr16.engineer@gmail.com>
2025-12-27 20:46:50 +0530

    commit

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -14,6 +14,7 @@ import AdminDashboard from "./pages/admin/AdminDashboard.jsx";
 import TechnicianDashboard from "./pages/technician/TechnicianDashboard.jsx";
 import EmployeeDashboard from "./pages/employee/EmployeeDashboard.jsx";
 
+
 // Technician Resolution Page
 import ResolutionPage from "./pages/technician/ResolutionPage.jsx";
 const App = () => {
